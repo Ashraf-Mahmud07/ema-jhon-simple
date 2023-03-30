12491e0750c0511db2476bcb4a69b03afa88a25a
Ashraf mahmud <ashrut444@gmail.com>
2023-03-30 12:48:21 +0600

    added products summary

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product);
-    const{img, name, price, seller, ratings}=props.product;
+    // console.log(props.product);
+    const { img, name, price, seller, ratings } = props.product;
+    const handleAddToCart=props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h4>{name}</h4>
-            <p>Price ${price}</p>
-            <p>Manufacturer :{seller}</p>
-            <p>Rating: {ratings} start</p>
-            <button>Add to cart</button>
+            <div className='product-info'>
+                <h4 className='product-name'>{name}</h4>
+                <p>Price ${price}</p>
+                <p>Manufacturer :{seller}</p>
+                <p>Rating: {ratings} stars</p>
+            </div>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'>Add to cart</button>
         </div>
     );
 };
